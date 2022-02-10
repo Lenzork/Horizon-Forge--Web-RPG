a75dce8363b9fdee9fa4ac57c89d1c071b302816
Lenzork <83922068+Lenzork@users.noreply.github.com>
2022-02-10 23:57:31 +0100

    default config resettet
    changed the main page a bit

--- config/config.js
+++ config/config.js
@@ -7,6 +7,6 @@ config.serverCount = 4;
 config.serverNames = ["Antropa", "Sinesta", "Landro", "Valis", "Valhalla"];
 config.serverPorts = [8888, 8889, 8890, 8891, 8892];
 config.serverSlots = [100, 80, 30, 100, 1000];
-config.startAllServersAtTheBeginning = false;
+config.startAllServersAtTheBeginning = true;
 
 module.exports = config;
